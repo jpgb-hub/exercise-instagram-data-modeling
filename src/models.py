92d05a2e8e2930d16b4fd32bdd5a06b6b5fe4203
Juan Pablo Guerrero <57576332+jpgb-hub@users.noreply.github.com>
2023-06-14 21:45:07 +0000

    guarda

--- src/models.py
+++ src/models.py
@@ -7,24 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Follower(Base):
+    __tablename__ = 'Follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, primary_key=True)
+    user_to_id = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    ID = Column(Integer, primary_key=True)
+    user = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)    
+    
 
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(Integer, primary_key=True)
+    type = Column(Integer)
+    url = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'Post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(Integer, primary_key=True)
+    user_id=Column(Integer) 
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column (Integer)
+    post_id = Column(Integer)
+
+    
     def to_dict(self):
         return {}
 
