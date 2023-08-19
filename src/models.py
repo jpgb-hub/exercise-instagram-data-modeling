c0c77c0b1188f19e7daa7ab3d82831da9eb268a3
Juan Pablo Guerrero <57576332+jpgb-hub@users.noreply.github.com>
2023-08-19 17:00:45 +0000

    relations

--- src/models.py
+++ src/models.py
@@ -1,22 +1,10 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import relationship, declarative_base, backref
 from eralchemy2 import render_er
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
-
-
-class Follower(Base):
-    __tablename__ = 'follower'
-    ID = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.ID'), nullable=False)
-    user_from = relationship("User", foreign_keys=[user_from_id], backref=backref("following", uselist=True))
-    user_to = relationship("User", foreign_keys=[user_to_id], backref=backref("followed_by", uselist=True))
 
 class User(Base):
     __tablename__ = 'user'
@@ -25,8 +13,14 @@ class User(Base):
     firstname = Column(String(250))
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    posts = relationship("Post", backref="user")
-    comments = relationship("Comment", backref="author")
+    # Las relaciones con Post y Comment se establecerán desde las otras clases
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    user_from = relationship("User", foreign_keys=[user_from_id], backref=backref("following", uselist=True))
+    user_to = relationship("User", foreign_keys=[user_to_id], backref=backref("followed_by", uselist=True))
 
 class Media(Base):
     __tablename__ = 'media'
@@ -39,12 +33,20 @@ class Media(Base):
 class Post(Base):
     __tablename__ = 'post'
     ID = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.ID')) 
-    comments = relationship("Comment", backref="post")
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    user = relationship("User", backref="posts")  # Relación con User
 
 class Comment(Base):
     __tablename__ = 'comment'
     ID = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('user.ID'))
-    post_id = Column(Integer, ForeignKey('post.ID')) 
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    post = relationship("Post", backref="comments")  # Relación con Post
+    author = relationship("User", backref="comments")  # Relación con User
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
